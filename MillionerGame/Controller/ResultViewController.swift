823be6b2da0150835688f021ec7207ac6aed8495
Maria Nesterova <ridebyhorse@gmail.com>
2024-02-27 01:02:54 +0300

    add resultController

--- MillionerGame/Controller/ResultViewController.swift
+++ MillionerGame/Controller/ResultViewController.swift
@@ -5,4 +5,25 @@
 //  Created by Иван Семенов on 25.02.2024.
 //
 
-import Foundation
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    private let resultView: ResultView
+    
+    init(questionNumber: Int, isCorrectAnswer: Bool) {
+        resultView = ResultView(questionNumber: questionNumber, isCorrectAnswer: isCorrectAnswer)
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.isHidden = true
+        view = resultView
+    }
+    
+}
